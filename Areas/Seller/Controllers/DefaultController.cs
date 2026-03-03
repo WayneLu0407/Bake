715b9fe0f4342859545d25675b54b5921ed7bb7d
Jessica <jessiev180@gmail.com>
2026-03-03 23:10:59 +0800

    增加seller area

---
+++ Areas/Seller/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bake.Areas.Seller.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Area("Seller")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
