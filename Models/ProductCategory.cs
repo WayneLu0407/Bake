007fd0fe0a1a0a616a33284b60c8a64d740205ab
jessiev180 <jessiev180@gmail.com>
2026-03-04 17:21:21 +0800

    add ew table Product Category (#13)

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bake.Models;
+
+public partial class ProductCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? CategoryDescription { get; set; }
+
+    public int DisplayOrder { get; set; }
+}
