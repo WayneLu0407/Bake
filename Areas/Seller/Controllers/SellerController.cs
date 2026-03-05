46d961397b0c9f5c99dcbc388d69acdce74e677b
Jessica <jessiev180@gmail.com>
2026-03-05 17:07:15 +0800

    create功能進行中

--- Areas/Seller/Controllers/SellerController.cs
+++ Areas/Seller/Controllers/SellerController.cs
@@ -1,47 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bake.Data;
+using Bake.Models;
+using Bake.Models.Sales;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace Bake.Areas.Seller.Controllers
 {
+    [Area("Seller")]
     public class SellerController : Controller
     {
-        [Area("Seller")]
+        private readonly BakeContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SellerController(BakeContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
         public IActionResult Dashboard()
         {
             return View();
         }
 
-        [Area("Seller")]
-        public IActionResult New()
+        public IActionResult New() => View();
+
+        // POST: Seller/New
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> New([Bind("ProductName,ProductDescription,CategoryId")] Product item)
         {
-            return View();
+            if (ModelState.IsValid) 
+            {
+                item.UserId = 1;
+                item.ProductDate = DateTime.Now;
+                var file = Request.Form.Files["ProductImage"];
+                if (file!= null && file.Length > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string savePath = Path.Combine(_env.WebRootPath, "ProductPicture", fileName);
+                    using (var stream = new FileStream(savePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    item.ProductImage = "ProductPicture/" + fileName;
+                }
+                else
+                {
+                    item.ProductImage = "ProductPicture/NoImage.jpg";
+                }
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Dashboard));
+
+            }
+            return View(item);
         }
 
-        [Area("Seller")]
+        
         public IActionResult Edit()
         {
             return View();
         }
-        [Area("Seller")]
+
         public IActionResult Orders()
         {
             return View();
         }
-        [Area("Seller")]
+
         public IActionResult Past()
         {
             return View();
         }
-        [Area("Seller")]
+
         public IActionResult Shop_settings()
         {
             return View();
         }
-        [Area("Seller")]
+
         public IActionResult Billing_account()
         {
             return View();
         }
-        [Area("Seller")]
+
         public IActionResult Shop()
         {
             return View();
