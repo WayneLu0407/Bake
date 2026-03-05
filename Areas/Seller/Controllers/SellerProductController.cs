27b9f3ca1c3e2a6ee267160c1931184cf877dcd4
jessiev180 <jessiev180@gmail.com>
2026-03-05 23:09:52 +0800

    Create new product jess (#20)
    * 新增商品畫面
    修改商品畫面
    
    * create功能進行中
    
    * create功能更新2
    
    * create功能finish

---
+++ Areas/Seller/Controllers/SellerProductController.cs
@@ -0,0 +1,71 @@
+﻿using Bake.Areas.Seller.ViewModels;
+using Bake.Data;
+using Bake.Models.Sales;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bake.Areas.Seller.Controllers
+{
+    [Area("Seller")]
+    public class SellerProductController : Controller
+    {
+
+        private readonly BakeContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SellerProductController(BakeContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult New() => View();
+
+        // POST: Seller/New
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> New([Bind("ProductName,ProductDescription,CategoryId,ProductImage")] ProductViewModel item)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = new Product
+                {
+                    ProductName = item.ProductName ?? "未命名商品",
+                    ProductDescription = item.ProductDescription,
+                    UserId = 1, // 暫時固定
+                    ProductDate = DateTime.Now,
+                    CategoryId = 1, // 暫時固定
+                    ProductMethod = "未設定"
+                };
+
+
+                
+                if (item.ProductImage != null && item.ProductImage.Length > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(item.ProductImage.FileName);
+                    string savePath = Path.Combine(_env.WebRootPath, "ProductPicture", fileName);
+                    using (var stream = new FileStream(savePath, FileMode.Create))
+                    {
+                        await item.ProductImage.CopyToAsync(stream);
+                    }
+                    product.ProductImage = "ProductPicture/" + fileName;
+                }
+                else
+                {
+                    product.ProductImage = "ProductPicture/NoImage.jpg";
+                }
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Dashboard", "SellerDashboard");
+
+            }
+            return View(item);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var BakeconnectionString = builder.Configuration.GetConnectionString("Bake");
 builder.Services.AddDbContext<BakeContext>(options =>
-    options.UseSqlServer(BakeconnectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Bake")));
 
 builder.Services.AddControllersWithViews();
 
