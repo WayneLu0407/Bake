6c7ab3d303a8e27eaf6798aac13b2da11943ddbc
jessiev180 <jessiev180@gmail.com>
2026-03-05 23:09:52 +0800

    Create new product jess (#20)
    * 新增商品畫面
    修改商品畫面
    
    * create功能進行中
    
    * create功能更新2
    
    * create功能finish

---
+++ Areas/Seller/ViewModels/ProductViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bake.Areas.Seller.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+
+        [Display(Name = "商品名稱")]
+        public string? ProductName { get; set; }
+
+        [Display(Name = "商品描述")]
+        public string? ProductDescription { get; set; }
+
+        [Display(Name = "商品圖片")]
+        public IFormFile? ProductImage { get; set; }
+        public string? ProductImagePath { get; set; }
+
+        [Display(Name = "商品分類")]
+        public int? CategoryId { get; set; }
+
+        [Display(Name = "商品價格")]
+        public decimal? ProductPrice { get; set; }
+
+        [Display(Name = "商品數量")]
+        public int? ProductQuantity { get; set; }
+    }
+}
---
+++ Partial/BakeContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Bake.Models;
+using Bake.Models.Platform;
+using Bake.Models.Sales;
+using Bake.Models.Service;
+using Bake.Models.Social;
+using Bake.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bake.Data;
+
+public partial class BakeContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Bake"));
+        }
+    }
+}
