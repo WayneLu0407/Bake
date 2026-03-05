21be3b9086ee8954ebab02cb7145ad51e7e61c62
Autumn Chu <autumn99770@gmail.com>
2026-03-05 23:01:53 +0800

    建立CartViewModel類別與欄位、cartcontroller

---
+++ ViewModel/CartViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Bake.ViewModel
+{
+    public class CartViewModel
+    {
+        public int ProdductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImgUrl { get; set; }
+        public int SubTotal 
+        {
+            get 
+            {
+                return Price * Quantity;
+            }
+        }
+    }
+}
