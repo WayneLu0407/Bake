c610e4fbd16f8cb80b861b8be208cbbddef0054f
Jessica <jessiev180@gmail.com>
2026-03-06 23:10:33 +0800

    商品分類頁 商品細節頁 所有商品頁

---
+++ Controllers/api/ProductsApiController.cs
@@ -0,0 +1,24 @@
+﻿using Bake.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bake.Controllers.api
+{
+    [Route("api/Product/[action]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly BakeContext _db;
+
+        public ProductApiController(BakeContext db)
+        {
+            this._db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var prod = _db.Products.ToList();
+            return Ok(prod);
+        }
+    }
+}
