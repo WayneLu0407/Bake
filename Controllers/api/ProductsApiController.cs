f43c8662674d07dedefe1f5f2bf0a7d1d83f9f4e
Jessica <jessiev180@gmail.com>
2026-03-07 13:31:50 +0800

    product index 更新

--- Controllers/api/ProductsApiController.cs
+++ Controllers/api/ProductsApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Bake.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bake.Controllers.api
 {
@@ -17,7 +18,24 @@ namespace Bake.Controllers.api
         [HttpGet]
         public IActionResult Get()
         {
-            var prod = _db.Products.ToList();
+            var prod = _db.Products
+                .Include(p => p.Category)        // 如果需要分類名稱
+                .Include(p => p.User)            // 透過 User 找到 Shop
+                    .ThenInclude(u => u.Shop)    // User → Shop
+                .Select(p => new {
+                    productId = p.ProductId,
+                    productName = p.ProductName,
+                    productImage = p.ProductImage,
+                    productRating = p.ProductRating,
+                    // ProductDetail 是 1對1，直接用導覽屬性
+                    productPrice = p.ProductDetail.ProductPrice,
+                    productDiscount = p.ProductDetail.ProductDiscount,
+                    productQuantity = p.ProductDetail.ProductQuantity,
+                    // Shop 透過 User 取得
+                    shopName = p.User.Shop.ShopName
+                })
+                .ToList();
+
             return Ok(prod);
         }
     }
