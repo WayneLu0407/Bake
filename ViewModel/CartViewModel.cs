f9eced418d0333b2b9c5c13508e5553ab1b5a310
Autumn Chu <autumn99770@gmail.com>
2026-03-07 16:38:58 +0800

    Vue cart

--- ViewModel/CartViewModel.cs
+++ ViewModel/CartViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CartViewModel
     {
-        public int ProdductId { get; set; }
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
