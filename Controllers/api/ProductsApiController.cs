67d3e5d323bfef499ad71d9f73c05aab0d236687
jessiev180 <jessiev180@gmail.com>
2026-03-07 14:54:55 +0800

    Buyer product index (#30)
    * 商品分類頁
    商品細節頁
    所有商品頁
    
    * product index更新
    
    * product index 更新
    
    * 更新商品列表

---
+++ Controllers/api/ProductsApiController.cs
@@ -0,0 +1,42 @@
+﻿using Bake.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bake.Controllers.api
+{
+    [Route("api/Product/[action]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly BakeContext _db;
+
+        public ProductApiController(BakeContext db)
+        {
+            this._db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var prod = _db.Products
+                .Include(p => p.Category)        // 如果需要分類名稱
+                .Include(p => p.User)            // 透過 User 找到 Shop
+                    .ThenInclude(u => u.Shop)    // User → Shop
+                .Select(p => new {
+                    productId = p.ProductId,
+                    productName = p.ProductName,
+                    productImage = p.ProductImage,
+                    productRating = p.ProductRating,
+                    // ProductDetail 是 1對1，直接用導覽屬性
+                    productPrice = p.ProductDetail.ProductPrice,
+                    productDiscount = p.ProductDetail.ProductDiscount,
+                    productQuantity = p.ProductDetail.ProductQuantity,
+                    // Shop 透過 User 取得
+                    shopName = p.User.Shop.ShopName
+                })
+                .ToList();
+
+            return Ok(prod);
+        }
+    }
+}
