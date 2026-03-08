6b6f3fb4a172a9945518de732e914b555136b808
Jessica <jessiev180@gmail.com>
2026-03-08 23:06:21 +0800

    details 頁面完成

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -29,10 +29,11 @@ namespace Bake.Controllers
 
 
 
-        //Products/Details
-        public IActionResult Details()
+        ///Products/Details/3
+        [Route("Products/Details/{id}")]
+        public IActionResult Details(int id)
         {
-            return View();
+            return View();  // 只負責回傳頁面，資料交給 API 處理
         }
     }
 }
