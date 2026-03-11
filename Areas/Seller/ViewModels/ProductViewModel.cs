635410099d37af08891fc4c519f80271b24fe053
jessiev180 <jessiev180@gmail.com>
2026-03-11 20:31:53 +0800

    Create new product jess (#55)
    * 新增商品畫面
    修改商品畫面
    
    * create功能進行中
    
    * create功能更新2
    
    * create功能finish
    
    * 修正viewimport
    
    * fix: 補回 app.UseSession()
    
    * 更新create功能
    
    * 更新create功能
    
    * 更新create功能0311

--- Areas/Seller/ViewModels/ProductViewModel.cs
+++ Areas/Seller/ViewModels/ProductViewModel.cs
@@ -8,27 +8,30 @@ namespace Bake.Areas.Seller.ViewModels
 
         [Required(ErrorMessage = "請輸入商品名稱")]
         [Display(Name = "商品名稱")]
-        public string? ProductName { get; set; }
+        public string ProductName { get; set; }
 
 
         [Required(ErrorMessage = "請輸入商品描述")]
         [Display(Name = "商品描述")]
-        public string? ProductDescription { get; set; }
+        public string ProductDescription { get; set; }
 
-        [Required(ErrorMessage = "請附上商品圖片")]
-        [Display(Name = "商品圖片")]
+
+        [Display(Name = "上傳新圖片")]
         public IFormFile? ProductImage { get; set; }
+
+        // 用來存放並顯示資料庫中現有的圖片路徑
+        [Display(Name = "目前圖片路徑")]
         public string? ProductImagePath { get; set; }
 
 
         [Required(ErrorMessage = "請選擇分類")]
         [Display(Name = "商品分類")]
-        public int? CategoryId { get; set; }
+        public int CategoryId { get; set; }
 
         [Range(1, 9999999999, ErrorMessage = "價格必須大於1")]
         [Required(ErrorMessage = "請輸入商品價格")]
         [Display(Name = "商品價格")]
-        public decimal? ProductPrice { get; set; }
+        public decimal ProductPrice { get; set; }
 
         [Range(0, 0.99, ErrorMessage = "折數必須介於 0 到 0.99 之間")]
         [Display(Name = "優惠折數(如: 不打折填 0，九折填 0.1)")]
@@ -37,7 +40,11 @@ namespace Bake.Areas.Seller.ViewModels
         [Required(ErrorMessage = "請輸入數量")]
         [Display(Name = "商品數量")]
         [Range(1,int.MaxValue, ErrorMessage = "數量必須大於 0")]
-        public int? ProductQuantity { get; set; }
+        public int ProductQuantity { get; set; }
+
+        [Required(ErrorMessage = "請輸入日期")]
+        [Display(Name = "商品過期日")]
+        public DateTime ExpireDate { get; set; }= DateTime.Now.AddDays(7);
     }
 
 }
