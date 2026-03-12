60b2550be84ae25acdb7492f4fe2fe4f76b75cbe
Lily Huang <lilyHuang1204@outlook.com>
2026-03-12 22:29:44 +0800

    SellerShopLily-商品編輯頁跟現有資料表欄位對接

---
+++ Areas/Seller/ViewModels/SellerShopSettingsReadViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Bake.Areas.Seller.ViewModels
+{
+
+    public class SellerShopSettingsReadViewModel
+    {
+        public string ShopName { get; set; } = string.Empty;
+
+        public string? ShopDescription { get; set; }
+
+        public string? ShopImg { get; set; }
+
+        public byte? StatusId { get; set; }
+    }
+}
