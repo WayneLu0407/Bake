fd135b44d120515bcede70e7c991194986574b8c
jessiev180 <jessiev180@gmail.com>
2026-03-12 20:37:09 +0800

    Edit product jess (#56)
    * edit
    
    * edit+all功能更新
    
    * 修改all
    
    * 改了edit controller
    還有all 的 view

--- ViewModel/ProductListViewModel.cs
+++ ViewModel/ProductListViewModel.cs
@@ -8,5 +8,8 @@
         public string? ProductDescription { get; set; }
         public string CategoryName { get; set; }
         public int ProductQuantity { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal? ProductDiscount { get; set; }
+        public DateTime? ExpireDate { get; set; }
     }
 }
