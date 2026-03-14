87398db0092a373334b6269c72ed7d0247df9ef5
Wayne Lu <abc840407@gmail.com>
2026-03-14 13:36:33 +0800

    新增 個人資訊變更功能

---
+++ Areas/Seller/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bake.Areas.Seller.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Email 為必填")]
+        [EmailAddress(ErrorMessage = "請輸入正確的 Email 格式")]
+        public string Email {  get; set; }
+        [Required(ErrorMessage = "請輸入密碼")]
+        [MinLength(6, ErrorMessage = "密碼最少需要 6 個字元")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "請輸入姓名")]
+        [StringLength(50,MinimumLength =2,ErrorMessage ="最少需要2個字元")]
+        public string Name {  get; set; }
+        
+    }
+}
