1ef06c3cf1cdcb64eabfe67fa9e08c4833022047
jessiev180 <jessiev180@gmail.com>
2026-03-27 19:20:48 +0800

    Post index jess (#106)
    更新搜尋後端篩選邏輯
    私訊連結

--- Controllers/api/PostApiController.cs
+++ Controllers/api/PostApiController.cs
@@ -113,64 +113,19 @@ namespace Bake.Controllers.api
             return Ok(list);
         }
 
-        // 取單筆商品 → /api/Post/GetById/3
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var prod = _db.Products
-                .Include(p => p.ProductDetail)
-                .Include(p => p.User).ThenInclude(u => u.Shop)
-                .Where(p => p.ProductId == id)
-                .Select(p => new {
-                    productId = p.ProductId,
-                    userId = p.UserId,
-                    productName = p.ProductName,
-                    productImage = p.ProductImage,
-                    productRating = p.ProductRating,
-                    productPrice = p.ProductDetail.ProductPrice,
-                    productDiscount = p.ProductDetail.ProductDiscount,
-                    productQuantity = p.ProductDetail.ProductQuantity,
-                    expireDate = p.ProductDetail.ExpireDate,
-                    shopName = p.User.Shop.ShopName,
-                    shopImg = p.User.Shop.ShopImg!= null? "/" + p.User.Shop.ShopImg.TrimStart('/'): null,
-                    productDescription = p.ProductDescription,
-                    ShelfLifeNote = p.ProductIngredient.ShelfLifeNote,
-                    Ingredient = p.ProductIngredient.Ingredients,
-                    NetWeight = p.ProductIngredient.NetWeight,
-                    categoryId = p.CategoryId,
-                    categoryName = p.Category.CategoryName,
-                })
-                .FirstOrDefault();
-
-            if (prod == null) return NotFound();
-            return Ok(prod);
-        }
-        //api/Post/Search?keyword = 巧克力
-        [HttpGet]
-        public IActionResult Search(string keyword)
-        {
-            if (string.IsNullOrWhiteSpace(keyword))
-                return Ok(Array.Empty<object>());
-            var results = _db.Products
-                .Include(p => p.ProductDetail)
-                .Where(p => p.ProductName.Contains(keyword))
-                .Take(8)
-                .Select(p => new
-                {
-                    ProductId=p.ProductId,
-                    ProductName=p.ProductName,
-                    ProductImage = p.ProductImage,
-                    ProductPrice= p.ProductDetail != null ? p.ProductDetail.ProductPrice : (decimal?)null
-                })
-                .ToList();
-            return Ok(results);
-        }
-
         // 全部文章+活動（給 PostList 頁用）
         [HttpGet("All")]
-        public async Task<IActionResult> All()
+        public async Task<IActionResult> All(string? keyword)
         {
-            var list = await BaseQuery()
+            var query = BaseQuery();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(p =>
+                p.Title.Contains(keyword) ||
+                p.Content.Contains(keyword));
+            }
+            var list = await query
                 .OrderByDescending(p => p.CreatedAt)
                 .Select(p => new
                 {
