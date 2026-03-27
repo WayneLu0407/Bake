d5c0a5a83bed6688b26dcb8ac9b067788722ad78
Autumn Chu <autumn99770@gmail.com>
2026-03-27 17:30:31 +0800

    調整運費機制

--- wwwroot/js/cart-hub.js
+++ wwwroot/js/cart-hub.js
@@ -24,7 +24,7 @@ const cartService = {
         const res = await fetch('/Cart/Remove', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ productId })
+            body: JSON.stringify(productId)
         });
         if (res.ok) {
             //await this.getItem();
@@ -39,7 +39,10 @@ const cartService = {
 // Vue 共用邏輯 放在Mix全域變數物件中
 const CartMixin = {
     data() {
-        return { cart: [] };
+        return {
+            cart: [],
+            shippingFee:120
+        };
     },
     // 1. computed > totalPrice、totalCount
     computed: {
@@ -61,12 +64,18 @@ const CartMixin = {
             this.cart = await cartService.getItem();
             //this.cart = data;
         },
-        //刪除商品
-        async deleteItem(productId) {
-            if (confirm('確定移除商品?')) {
-                await cartService.removeItem(productId);
+
+        //加入購物車
+        async addCart(product) {
+            const selectedQty = product.qty || 1;
+            const addSuccess = await cartService.updateQty(product.productId, selectedQty);
+
+            if (addSuccess) {
+                product.quantity = 1; //把畫面數字重設回1
+                alert(`商品${product.productName}選購${selectedQty}件 已加入購物車!`);
             }
         },
+        
         //全域通用的數量加減邏輯
         async updateQuantity(productId, change) {
             const item = this.cart.find(i => i.productId === productId);
@@ -75,7 +84,19 @@ const CartMixin = {
             } else {
                 await cartService.updateQty(productId, change);
             }
-        } 
+        },
+
+        //刪除商品
+        async deleteItem(productId) {
+            if (confirm('確定移除商品?')) {
+                await cartService.removeItem(productId);
+            }
+        },
+
+        formatNumber(num) {
+            if (!num) return '0';
+            return num.toLocaleString('zh-TW'); // 強制台灣格式
+        }
     },
     //3. mounted 掛上addEventListener
     mounted() {
