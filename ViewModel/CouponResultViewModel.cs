729b8bfc537cc82d81614842815bb54c3a62f7a3
Autumn Chu <autumn99770@gmail.com>
2026-04-04 22:32:15 +0800

    買家購物車輸入優惠券

---
+++ ViewModel/CouponResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Bake.ViewModel
+{
+    public class CouponResultViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+    }
+
+    public class CouponApplyRequest
+    {
+        public string CouponCode { get; set; }
+    }
+}
