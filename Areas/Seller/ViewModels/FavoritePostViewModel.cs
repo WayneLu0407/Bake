a11f831fd6af136017cff0754eb948097bab7b6b
Wayne Lu <abc840407@gmail.com>
2026-04-09 12:51:54 +0800

    新增最愛商品頁面 收藏貼文頁面&& 誰追蹤我

---
+++ Areas/Seller/ViewModels/FavoritePostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Bake.Models.Social;
+using Bake.Models.User;
+
+namespace Bake.Areas.Seller.ViewModels
+{
+    public class FavoritePostViewModel
+    {
+        public List<Post> MySavePosts { get; set; } = new List<Post>();
+        public List<UserProfile> MyFollowers { get; set; } = new List<UserProfile>();
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int SavedCount { get; set; }
+        public int FollowersCount => MyFollowers.Count;
+    }
+}
